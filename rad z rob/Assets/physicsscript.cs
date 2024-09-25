353c84396d2ab343e44ef893a611d26e0f3eab1f
STUDENT Jack O Mahoney <Jack.OMahoney@students.ittralee.ie>
2024-09-25 12:52:11 +0100

    Signed-off-by: STUDENT Jack O Mahoney <Jack.OMahoney@students.ittralee.ie>

---
+++ rad z rob/Assets/physicsscript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class physicsscript : MonoBehaviour
+{
+    Rigidbody rb; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+        if (rb == null)
+            print("found rigid body");
+        else
+            print("could not find rigid body");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            rb.AddExplosionForce(500,
+                transform.position +new Vector3(0,-1,0),  2);
+    }
+}
