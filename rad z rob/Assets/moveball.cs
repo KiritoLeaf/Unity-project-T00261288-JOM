2ed0ac3d9e4514e5af94db551f01897f5f88b76c
STUDENT Jack O Mahoney <Jack.OMahoney@students.ittralee.ie>
2024-10-02 12:50:57 +0100

    Signed-off-by: STUDENT Jack O Mahoney <Jack.OMahoney@students.ittralee.ie>

--- rad z rob/Assets/moveball.cs
+++ rad z rob/Assets/moveball.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class moveball : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
+        if (animator == null)
+            print("could not find aninmator component");
+        else
+            print("animator component found");
         
     }
 
@@ -14,8 +20,15 @@ public class moveball : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
-        transform.position += transform.forward * Time.deltaTime;
-
+        {
+            transform.position += transform.forward * Time.deltaTime;
+            animator.SetBool("isRunning", true);
+        
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("isRunning", false);
+        }
         if (Input.GetKey(KeyCode.A))
             transform.Rotate(Vector3.up, 30* Time.deltaTime);
 
